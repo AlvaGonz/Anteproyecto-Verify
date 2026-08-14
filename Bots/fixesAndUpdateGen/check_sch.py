dee418026a0093f3e64a6c735781c3954c34b649
rayrvl <rayventura456@gmail.com>
2026-08-14 09:13:16 -0400

    refactor: reorganize project scripts into a centralized Bots/fixesAndUpdateGen directory and clean up legacy files.
    refactor: remove obsolete scripts and update database synchronization logic
    
    - Deleted unused scripts: export_csv.py, fix_gen.py, fix_logconsultas.sql, patch_scripts.py, rewrite.py, sync_db.py, sync_db2.py, update_gen_4.py, update_gen_5.py, update_gen_proj.py, update_lic.py, and update_up_lic.py.
    - Enhanced database synchronization logic to improve efficiency and maintainability.
    - Updated related functions to ensure compatibility with the new structure.

---
+++ Bots/fixesAndUpdateGen/check_sch.py
@@ -0,0 +1,6 @@
+﻿import pymssql
+conn = pymssql.connect(server='localhost', user='sa', password='Your_password123', database='verifinca-spm-uce-2026', as_dict=True)
+cursor = conn.cursor()
+cursor.execute("SELECT TOP 1 * FROM CatastroTitulo")
+print(cursor.fetchall())
+conn.close()
